78f79494da430a67e8acc5b6a0485f3a55819a36
Kareem Gurung <66091354+kareemgurung@users.noreply.github.com>
2023-08-30 13:29:10 +0100

    Header component completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,75 @@
-const Header = () => {
-    return <div>Header</div>
-}
+import { Link, useNavigate } from "react-router-dom";
+import styles from "./header.module.scss";
+import { useState } from "react";
 
-export default Header
+const Header = () => {
+  const navigate = useNavigate();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const showMenuHandler = () => {
+    setShowMenu((prevState) => !prevState);
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link to="/" className={styles.logo}>
+        <img src="../../../images/logo.svg" alt="my team logo" />
+      </Link>
+      <button
+        type="button"
+        aria-label="show menu button"
+        className={styles.iconButton}
+        onClick={() => showMenuHandler()}
+      >
+        <img src="../../../images/icon-hamburger.svg" alt="hamburger icon" />
+      </button>
+      <div
+        className={
+          showMenu
+            ? `${styles.container} ${styles.active}`
+            : `${styles.container}`
+        }
+      >
+        <button
+          type="button"
+          aria-label="close menu button"
+          className={`${styles.iconButton} ${styles.closeButton}`}
+          onClick={() => showMenuHandler()}
+        >
+          <img src="../../../images/icon-close.svg" alt="hamburger icon" />
+        </button>
+        <nav>
+          <li>
+            <Link to="/" onClick={() => showMenuHandler()}>
+              home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={() => showMenuHandler()}>
+              about
+            </Link>
+          </li>
+        </nav>
+        <button
+          type="button"
+          onClick={() => {
+            navigate("/contact");
+            showMenuHandler();
+          }}
+          className={styles.contactButton}
+        >
+          contact us
+        </button>
+      </div>
+      <div
+        className={
+          showMenu
+            ? `${styles.backdrop} ${styles.showBackdrop}`
+            : `${styles.backdrop}`
+        }
+      ></div>
+    </header>
+  );
+};
+
+export default Header;
